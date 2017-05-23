b66488ef9ca7d7e6190f24270d78fa9c5f1f6f38
Peter <neu20133788@163.com>
2017-05-23 15:38:54 +0800

    毕设版本5.0：添加搜索功能

---
+++ src/com/peter/servlet/GetSearchNoticesServlet.java
@@ -0,0 +1,54 @@
+package com.peter.servlet;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.peter.bean.Notice;
+import com.peter.bean.Trade;
+import com.peter.dao.BeanDao;
+import com.peter.dao.BeanDaoImpl;
+import com.peter.result.NetReturn;
+import com.peter.result.Result;
+
+@WebServlet("/GetSearchNotices")
+public class GetSearchNoticesServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		System.out.println("Õ¯¬Á∑√Œ ≥…π¶");
+		String query = request.getParameter("query");
+		System.out.println(query);
+		
+		BeanDao dao = new BeanDaoImpl();
+		String hql = "FROM Notice n WHERE n.title like '%" + query + "%'";
+		Result<List<Notice>> result = new Result<>();
+		result.result(NetReturn.SUCCESS);
+		List<Notice> notices = dao.findByHQL(hql);
+		if (!notices.isEmpty()) {
+			result.setData(notices);
+		} else {
+			result.setData(null);
+		}
+		
+		String json = new Gson().toJson(result);
+		System.out.println(json);
+		response.setContentType("application/json; charset=utf-8");  
+        response.setCharacterEncoding("UTF-8"); 
+        OutputStream out = response.getOutputStream();  
+        out.write(json.getBytes("UTF-8"));  
+        out.flush(); 
+	}
+}
